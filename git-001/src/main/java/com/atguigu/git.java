1f02bd23328229c6c222f3c5eb91fba65b97475a
xfy <1844960380@qq.com>
2022-05-28 20:18:53 +0800

    hot-fix commit02

--- git-001/src/main/java/com/atguigu/git.java
+++ git-001/src/main/java/com/atguigu/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("hello git 02");
         System.out.println("hello git 03");
         System.out.println("hello git 04");
+        System.out.println("hello git merge01");
     }
 }
